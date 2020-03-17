e160efb167f92d311444291bbe1da06316d580ac
sofiasolomon0914 <ssolomon2770@eagle.fgcu.edu>
2020-03-17 11:28:30 -0400

    Commit 2

--- ForLoop/src/academy/learnprogramming/Main.java
+++ ForLoop/src/academy/learnprogramming/Main.java
@@ -12,9 +12,42 @@ public class Main {
         for(int i=0; i<5; i++){
             System.out.println("Loop " + i + " hello!");
         }
+
+        for(int i=8; i>1; i--){
+            System.out.println("10,000 at "+ i +"% interest = "+ calculateInterest(10000.0, i));
+        }
+
+        int count = 0;
+        for(int i=10; i<50; i++) {
+            if(isPrint(i)){
+                count++;
+                System.out.println("Number "+ i +" is a prime number");
+                if(count == 3){
+                    System.out.println("Exciting for loop");
+                    break;
+                }
+            }
+        }
     }
+    public static boolean isPrint(int n){
+
+        if(n==1){
+            return false;
+        }
+
+        for(int i=2; i<=n/2; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     public static double calculateInterest(double amount, double interestRate){
         return(amount * (interestRate/100));
     }
+
+
+
 }
